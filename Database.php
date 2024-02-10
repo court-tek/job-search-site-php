a4d6e23542bba04a5c003ef625c442423cd9d4ec
court-tek <dishoughng@gmail.com>
2024-02-10 16:11:47 -0500

    new updates

---
+++ Database.php
@@ -0,0 +1,27 @@
+<?php
+
+class Database {
+    public $conn;
+
+    /**
+     * Constructor for Database class
+     * 
+     * @param array $config
+     */
+    public function __construct($config)
+    {
+        $dsn = "mysql:host={$config['host']};port={$config['port']};dbname={$config['dbname']}";
+        
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ];
+        
+        try {
+            $this->conn = new PDO($dsn, $config['username'], $config['password'], $options);
+            echo "Connected to database";
+        } catch (PDOException $e) {
+            throw new Exception("Connection failed:{$e->getMessage()}");
+        }
+    }
+}
