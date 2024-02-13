01624a0fa05b15cadda2e00f7174c636f4c53e3e
court-tek <dishoughng@gmail.com>
2024-02-13 09:36:08 -0500

    added the session start method

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -143,7 +143,9 @@ class ListingController
         }
 
         $this->db->query('DELETE FROM listings WHERE id = :id', $params); 
-
+        
+        // Set flash message
+        $_SESSION['success_message'];
         redirect('/listings');
     }
 
