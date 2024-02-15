073d83677d12f860c4defff88c0104c51bd57302
court-tek <dishoughng@gmail.com>
2024-02-15 14:56:20 -0500

    header styles added

--- App/views/partials/navigation.php
+++ App/views/partials/navigation.php
@@ -1,18 +1,25 @@
-<?php 
-
-    
-?>
 <!-- Nav -->
-<header class="bg-blue-900 text-white p-4">
-    <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-3xl font-semibold">
-            <a href="/">endeed</a>
-        </h1>
-        <nav class="space-x-4">
-            <a href="login.html" class="text-white hover:underline">Login</a>
-            <a href="register.html" class="text-white hover:underline">Register</a>
-            <a href="/listings/new" class="<?php echo ($_SERVER['REQUEST_URI'] == '/listings/new') ? 'invisible' : '' ?> bg-yellow-500 hover:bg-yellow-600 text-black px-4 py-2 rounded hover:shadow-md transition duration-300">
-            <i class="fa fa-edit"></i>Post a Job</a>
+<header class="header">
+    <div class="header__container">
+        <div class="header__navigationL">
+            <h1 class="header__logo">
+                <a href="/">
+                    endeed
+                </a>
+            </h1>
+            <div class="header__menu">
+                <a href="#" class="header__menu-link">Home</a>
+                <a href="#" class="header__menu-link">Company reivews</a>
+                <a href="#" class="header__menu-link">Find salaries</a>
+            </div>
+        </div>
+        
+        <nav class="header__account">
+            <a href="login.html" class="header__signin-link">Sign in</a>
+            <!-- <a href="register.html" class="">Register</a> -->|
+            <a href="/listings/new" class="<?php echo ($_SERVER['REQUEST_URI'] == '/listings/new') ? 'invisible' : '' ?> header__new-post-link">
+                Employers / Post Job
+            </a>
         </nav>
     </div>
 </header>
